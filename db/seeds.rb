cef76ace773e303ec60a5d7c0261ab56c8505391
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-03-07 08:18:08 -0500

    moved dir

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.destroy_all
+Answer.destroy_all
+
+q1 = Question.create(title:"Testing", body: "thiiiiiiiings")
+
+q2 = Question.create(title:"Testing2", body: "thiiiiiiiings2")
+
+q3 = Question.create(title:"Testing3", body: "thiiiiiiiings3")
+
+
+a1 = Answer.create(title:"stuff", body:"other stuff")
+
+a2 = Answer.create(title:"stuff2", body:"other stuff2")
+
+a3 = Answer.create(title:"stuff3", body:"other stuff3")
